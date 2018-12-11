661910382baf005809c2083e63b2eb08e9fa3f22
nbochejny <nbochejny@gmail.com>
2018-12-12 00:16:37 +0100

    Aufgabe5

--- src/Book.java
+++ src/Book.java
@@ -29,5 +29,20 @@ public class Book {
     public String getIsbn(){
         return isbn;
     }
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public void setReleased(Date released) {
+        this.released = released;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 
 }
